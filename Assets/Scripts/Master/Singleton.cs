1631dca6b19c6022e57350a283ff78eb4b0268d7
fruitymario <teamtunagames@gmail.com>
2023-05-26 20:43:20 -0400

    Singleton

---
+++ Assets/Scripts/Master/Singleton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    public static T Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Instance = this as T;
+    }
+}
