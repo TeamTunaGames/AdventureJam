fc5a13b8378054ccf0d9ca815eaac33b61e2dc7c
fruitymario <teamtunagames@gmail.com>
2023-05-26 22:18:28 -0400

    Added Controller

---
+++ Assets/Scripts/Master/ControllerManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager : Singleton<ControllerManager>
+{
+    public PlayerInput Controller { get; private set; }
+    public PlayerInput.MainGameActions Actions { get; private set; }
+
+    protected new void Awake()
+    {
+        base.Awake();
+        if(Controller == null)
+        {
+            CreateController();
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        if(Controller != null)
+        {
+            Controller.Enable();
+        }
+        else
+        {
+            CreateController();
+            Controller.Enable();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(Controller != null)
+        {
+            Controller.Disable();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(Controller != null)
+        {
+            Controller.Dispose();
+        }
+    }
+
+    private void CreateController()
+    {
+        Controller = new();
+        Actions = Controller.MainGame;
+    }
+
+}
--- Assets/Scripts/Master/Singleton.cs
+++ Assets/Scripts/Master/Singleton.cs
@@ -7,7 +7,7 @@ public class Singleton<T> : MonoBehaviour where T : Component
     public static T Instance { get; private set; }
 
 
-    private void Awake()
+    protected void Awake()
     {
         if(Instance != null)
         {
