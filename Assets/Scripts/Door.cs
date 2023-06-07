d85c616b564a735bba78a87d2da2a3f3ff55e82f
fruitymario <teamtunagames@gmail.com>
2023-06-07 17:21:03 -0400

    Added arcade

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour, IInteractable
 {
     [field:SerializeField] public bool IsInteractable { get; private set; }
-    public SceneReference scene;
+    [SerializeField] private SceneReference scene;
+    [SerializeField] private int spawnLocation = 0;
 
     public void Interact()
     {
-        SceneManager.LoadScene(scene);
+        GameMaster.Instance.GoToScene(scene, spawnLocation);
     }
 }
